af9ead181bb43f4f08f686a35d204d0fce7b50d3
andi0b <andix25@gmail.com>
2018-12-02 18:48:30 +0100

    Completed Day 1

--- Day1801/Day1801.cs
+++ Day1801/Day1801.cs
@@ -8,31 +8,54 @@ namespace AdventOfCode.Day1801
 {
     public class Day1801Solution : SolutionBase<int[], int[]>
     {
+        public override IEnumerable<int[]> GetPart1SampleInputs() => new[]
+                                                                     {
+                                                                         new[] {+1, +1, +1},
+                                                                         new[] {+1, +1, -2},
+                                                                         new[] {-1, -2, -3},
+                                                                     };
+
+        public override IEnumerable<string> GetPart1SampleOutputs() => new[]
+                                                                       {
+                                                                           3,
+                                                                           0,
+                                                                           -6
+                                                                       }.Select(x => x.ToString());
+
+
+        public override IEnumerable<int[]> GetPart2SampleInputs() => new[]
+                                                                     {
+                                                                         new[] {+1, -1},
+                                                                         new[] {+3, +3, +4, -2, -4},
+                                                                         new[] {-6, +3, +8, +5, -6},
+                                                                         new[] {+7, +7, -2, -7, -4},
+                                                                     };
+
+        public override IEnumerable<string> GetPart2SampleOutputs() => new[]
+                                                                       {
+                                                                           0,
+                                                                           10,
+                                                                           5,
+                                                                           14
+                                                                       }.Select(x => x.ToString());
+
+
         public override int[] GetPart1Input() => ReadInputLines().Select(int.Parse).ToArray();
 
-        public override object SolvePart1(int[] input)
-        {
-            var result = 0;
-            foreach (var i in input)
-                result += i;
-
-            return result;
-        }
+        public override object SolvePart1(int[] input) => input.Sum();
 
         public override object SolvePart2(int[] input)
         {
-            var pastResults = new List<int>();
             var result = 0;
+            var pastResults = new HashSet<int>();
+            pastResults.Add(0);
             while (true)
                 foreach (var i in input)
                 {
                     result += i;
-                    if (pastResults.Contains(result))
-                    {
-                        return result;
-                    }
 
-                    pastResults.Add(result);
+                    if (!pastResults.Add(result))
+                        return result;
                 }
         }
     }
