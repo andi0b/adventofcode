87ac05c6993393506de6fcd2e0f675b9527b3129
andi0b <andix25@gmail.com>
2018-12-05 21:45:50 +0100

    Further simplified Day 5

--- Day1805/Day1805.cs
+++ Day1805/Day1805.cs
@@ -14,12 +14,8 @@ namespace AdventOfCode.Day1805
         public override IEnumerable<string> GetPart2SampleOutputs() => new[] {"4"};
         public override string GetPart1Input() => ReadInput();
 
-        private static bool DoReact(char a, char b)
-        {
-            var sameCharacter = char.ToLowerInvariant(a) == char.ToLowerInvariant(b);
-            var differentPolarity = char.IsUpper(a) && char.IsLower(b) || char.IsLower(a) && char.IsUpper(b);
-            return sameCharacter && differentPolarity;
-        }
+        private static bool CanReact(char a, char b)
+            => a != b && char.ToLowerInvariant(a) == char.ToLowerInvariant(b);
 
         private static List<char> DoReactions(IEnumerable<char> input)
         {
@@ -28,7 +24,7 @@ namespace AdventOfCode.Day1805
             foreach (var next in input)
             {
                 var previous = list.LastOrDefault();
-                if (previous != default && DoReact(previous, next))
+                if (previous != default && CanReact(previous, next))
                 {
                     list.RemoveAt(list.Count - 1);
                 }
@@ -41,26 +37,18 @@ namespace AdventOfCode.Day1805
             return list;
         }
 
-        private static List<char> DoAllReactions(List<char> reduced)
-        {
-            while (reduced.Count != (reduced = DoReactions(reduced)).Count){}
-            return reduced;
-        }
-
-        public override object SolvePart1(string input) => DoAllReactions(input.ToList()).Count;
+        public override object SolvePart1(string input) => DoReactions(input.ToList()).Count;
 
         public override object SolvePart2(string input)
         {
-            var allUppercaseLetters = from charCode in Enumerable.Range(65, 26)
-                                      select (char) charCode;
+            var allUppercaseLetters = from charCode in Enumerable.Range('A', 26)
+                                      select ((char) charCode).ToString();
 
             var inputsWithoutOneChar = from uppercaseLetter in allUppercaseLetters
-                                       let lowercaseLetter = char.ToLowerInvariant(uppercaseLetter)
-                                       select input.Replace(uppercaseLetter.ToString(), string.Empty)
-                                                   .Replace(lowercaseLetter.ToString(), string.Empty);
+                                       select input.Replace(uppercaseLetter, string.Empty, StringComparison.InvariantCultureIgnoreCase);
 
             var reduced = from i in inputsWithoutOneChar
-                          select DoAllReactions(i.ToList()).Count;
+                          select DoReactions(i.ToList()).Count;
 
             return reduced.Min(x => x);
         }
